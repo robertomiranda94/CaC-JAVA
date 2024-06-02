556267b7542d3593ec45357d6bbaa403f45c2397
Roberto <mirandaroberto1994@gmail.com>
2024-06-02 20:37:47 -0300

    implementa api a la seccion de tendencias

---
+++ js/peliculas.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const apiKey = "TMDB_API_KEY";
+    const tendenciasContainer = document.getElementById("tendencias-container");
+
+    // Función para obtener las tendencias del día
+    async function getTendencias() {
+        try {
+            const response = await fetch(
+                `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}&language=es-ES`
+            );
+            const data = await response.json();
+            displayTendencias(data.results);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+
+    function displayTendencias(movies) {
+        tendenciasContainer.innerHTML = "";
+        movies.forEach((movie) => {
+            const movieCard = document.createElement("div");
+            movieCard.classList.add("card", "col-md-4", "fade-in");
+            movieCard.innerHTML = `
+          <a href="#">
+            <div class="img1">
+              <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="card-img-top" alt="${movie.title}">
+            </div>
+            <div class="img2"></div>
+            <div class="title">${movie.title}</div>
+            <div class="text">${movie.overview}</div>
+            <a href="#">
+              <div class="catagory">Películas <i class="fas fa-film"></i></div>
+            </a>
+            <a href="#">
+              <div class="views">${movie.vote_count} <i class="far fa-eye"></i> </div>
+            </a>
+          </a>
+        `;
+            tendenciasContainer.appendChild(movieCard);
+        });
+    }
+
+    getTendencias();
+});
